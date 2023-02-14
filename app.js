af8e07da586c29db47951d305d831ab533d59b58
SD-PARK <tkdeh129@naver.com>
2023-02-14 18:50:39 +0900

    feat($browser): register page

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('./server/config/io')(server);
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/html/index.html');
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/client/html/login.html');
+});
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/client/html/register.html');
 });
 
-app.use(express.static('client'));
+app.use('/client', express.static(path.join(__dirname, 'client')));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 // app.use(express.urlencoded({extended: false}));
 // app.set('io', io);
 
--- client/js/index.js
+++
@@ -1,6 +0,0 @@
-$(() => {
-    console.log('1');
-    const socket = io();
-    console.log('2');
-    socket.emit('login');
-});
