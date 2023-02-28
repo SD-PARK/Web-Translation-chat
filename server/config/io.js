6013669f045443b983ea0be47ca2961a53fa883b
SD-PARK <tkdeh129@naver.com>
2023-02-28 17:55:47 +0900

    feat: 친구 추가 시 대화방 생성

--- server/config/io.js
+++ server/config/io.js
@@ -8,32 +8,61 @@ module.exports = (server, session) => {
     //const friendsList = require('../src/friendsListSocket')(io, db);
     io.on('connection', (socket) => {
         let userId;
+
+        // Main Page 접속
         socket.on('login', (callback) => {
             userId = socket.handshake.session.USER_ID;
             console.log('Client Login!: ' + userId);
-            db.query(`CALL PRINT_USER_INFO(${userId})`, (err, info) => {
-                callback(info[0][0]);
-            });
+            db.query(`CALL PRINT_USER_INFO(${userId})`, (err, info) => { callback(info[0][0]); });
         });
 
+        // 친구 목록 호출
         socket.on('friendList', (callback) => {
-            db.query(`CALL PRINT_FRIENDS_INFO(${userId})`, (err, friends) => {
-                callback(friends[0]);
-            });
+            db.query(`CALL PRINT_FRIENDS_INFO(${userId})`, (err, friends) => { callback(friends[0]); });
         });
 
+        // 친구 추가(와 동시에 채팅방 생성)
         socket.on('addFriend', (info, callback) => {
             db.query(`CALL SEARCH_USER(${userId}, '${info.NAME}', '${info.TAG}')`, (err, targetId) => {
-                console.log(targetId[0]);
                 if(targetId[0][0]) {
-                    db.query(`CALL ADD_FRIEND(${userId}, ${targetId[0][0].USER_ID})`, (err, res) => {
-                        callback(1);
-                    });
+                    console.log(targetId[0][0]);
+                    let roomId = makeRoom('ONE');
+                    try {
+                        db.beginTransaction();
+                        db.query(`CALL ADD_FRIEND(${userId}, ${targetId[0][0].USER_ID}, '${roomId}')`, () => { callback(1); });
+                        console.log('Add Relation: ', userId, '=>', targetId[0][0].USER_ID);
+                        db.commit();
+                    } catch (err) {
+                        db.rollback();
+                    }
                 } else {
                     callback(0);
                 }
             });
         });
+
+        /** ONE, MANY ; RoomID 반환 */
+        function makeRoom(status) {
+            let roomId = randomChars(9);
+            try {
+                db.beginTransaction();
+                db.promise().query(`CALL CREATE_ROOM('${roomId}', '${status}')`);
+                console.log('Create Room:', roomId);
+                db.commit();
+            } catch (err) {
+                db.rollback();
+            }
+            return roomId;
+        }
+
+        function randomChars(length) {
+            const chars = '0123456789abcdefghijkmnopqrstuvwxyzABCDEFGHIJKMNOPQRSTUVWXYZ';
+            let str = '';
+            for(let i=0; i<length; i++) {
+                str += chars.charAt(Math.floor(Math.random() * 60));
+            }
+            return str;
+        }
     });
 
     return io;
--- server/src/loginController.js
+++ server/src/loginController.js
@@ -37,46 +37,43 @@ exports.registerGetMid = (req, res) => {
 exports.registerPostMid = (req, res) => {
     const {email, password, name} = req.body;
     const crypto_pw = hash(password);
-    
-    try {
-        db.beginTransaction();
         
-        db.query(`CALL CHECK_DUPLICATE_EMAIL('${email}')`, (err, check) => {
-            if (check[0][0]) {
-                console.log(check[0][0]);
-                res.redirect('/register?err=100');
-            }
-            else {
-                db.query(`CALL CHECK_DUPLICATE_NAMETAG('${name}')`, (err, used_tag) => {
-                    let tag;
-                    if (used_tag[0][0]) {
-                        if (used_tag[0].length == 9999) {
-                            // 모든 네임태그 사용 중일 때
-                        } else {
-                            do {
-                                tag = ('#' + Math.floor(Math.random() * 10000));
-                                for(let i of used_tag[0])
-                                    if (tag == i.NAME_TAG) {
-                                        tag = -1;
-                                    }
-                            } while (tag == -1);
-                        }
+    db.query(`CALL CHECK_DUPLICATE_EMAIL('${email}')`, (err, check) => {
+        if (check[0][0]) {
+            console.log(check[0][0]);
+            res.redirect('/register?err=100');
+        }
+        else {
+            db.query(`CALL CHECK_DUPLICATE_NAMETAG('${name}')`, (err, used_tag) => {
+                let tag;
+                if (used_tag[0][0]) {
+                    if (used_tag[0].length == 9999) {
+                        // 모든 네임태그 사용 중일 때
                     } else {
-                        tag = ('#' + Math.floor(Math.random() * 10000));
+                        do {
+                            tag = ('#' + Math.floor(Math.random() * 10000));
+                            for(let i of used_tag[0])
+                                if (tag == i.NAME_TAG) {
+                                    tag = -1;
+                                }
+                        } while (tag == -1);
                     }
-                    
-                    db.query(`CALL SIGNUP('${email}', '${crypto_pw}', '${name}', '${tag}')`, (err, r) => {
+                } else {
+                    tag = ('#' + Math.floor(Math.random() * 10000));
+                }
+                
+                try {
+                    db.beginTransaction();
+                    db.query(`CALL SIGNUP('${email}', '${crypto_pw}', '${name}', '${tag}')`, () => {
                         res.redirect('/login');
                         console.log('register:', email, name + tag);
                     });
-                });
-            }
-        });
-    } catch (err) {
-        console.log(err);
-        res.redirect('/register?err=101');
-        db.rollback();
-    } finally {
-        db.commit();
-    }
+                    db.commit();
+                } catch (err) {
+                    db.rollback();
+                }
+            });
+        }
+    });
+    db.commit();
 }
