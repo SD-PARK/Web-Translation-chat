f19e106b79817c61b909959e75e18cdf3460e3ae
SD-PARK <tkdeh129@naver.com>
2023-03-10 23:05:51 +0900

    fix: 디폴트 언어 설정

--- client/js/multiLanguage_login.js
+++ client/js/multiLanguage_login.js
@@ -91,5 +91,5 @@ function selectLanguage() {
 }
 
 setLanguage(getCookie('setLang')??'ko');
-$('div#langForm > select').val(getCookie('setLang')).prop('selected', true); // 페이지 언어 설정
-$('form#register > select').val(getCookie('setLang')).prop('selected', true); // register 내 회원 정보의 언어 설정
+$('div#langForm > select').val(getCookie('setLang')??'ko').prop('selected', true); // 페이지 언어 설정
+$('form#register > select').val(getCookie('setLang')??'ko').prop('selected', true); // register 내 회원 정보의 언어 설정
