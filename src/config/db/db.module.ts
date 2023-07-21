71e3d0df94bbecee472a9f9855d9a5d3bf27255e
SD-PARK <tkdeh129@naver.com>
2023-07-21 14:15:34 +0900

    feat: DB(MySQL) 연동

---
+++ src/config/db/db.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { ChatRoom } from '../../modules/chat_rooms/chat_rooms.entity';
+import { ChatMessage } from '../../modules/chat_messages/chat_messsages.entity';
+
+@Module({
+  imports: [TypeOrmModule.forRootAsync({
+    useFactory: async (configService: ConfigService) => ({
+      type: 'mysql',
+      host: configService.get('DB_HOST'),
+      port: configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_NAME'),
+      entities: [ChatRoom, ChatMessage],
+      timezone: '+00:00',
+      synchronize: false,
+    }),
+    inject: [ConfigService],
+  })],
+})
+export class DBModule {}
