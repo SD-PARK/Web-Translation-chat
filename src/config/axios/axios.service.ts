9d43510523c2ac608d1604d140a8f40df10ff366
SD-PARK <tkdeh129@naver.com>
2023-07-21 17:28:46 +0900

    feat: axios 모듈 작성

---
+++ src/config/axios/axios.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+@Injectable()
+export class AxiosService {
+    async get(url: string): Promise<AxiosResponse> {
+        try { return await axios.get(url); }
+        catch(err) { throw err; }
+    }
+
+    async post(url: string, data: any, config: AxiosRequestConfig<any>): Promise<AxiosResponse> {
+        try { return await axios.post(url, data, config); }
+        catch(err) { throw err; }
+    }
+
+    async put(url: string, data: any): Promise<AxiosResponse> {
+        try { return await axios.put(url, data); }
+        catch(err) { throw err; }
+    }
+
+    async delete(url: string): Promise<AxiosResponse> {
+        try { return await axios.delete(url); }
+        catch(err) { throw err; }
+    }
+}
