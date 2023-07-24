88e653c714f764dd6adb61b4f641456c39d4f422
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-07-24 09:07:55 +0900

    refactor: axios 요청 시 예외 처리

--- src/api/papago/papago.service.ts
+++ src/api/papago/papago.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AxiosService } from 'src/config/axios/axios.service';
 
+const languageCode: string[] = ['ko', 'en', 'ja', 'zh-CN', 'zh-TW'];
+
 @Injectable()
 export class PapagoService {
     constructor(
@@ -13,27 +15,30 @@ export class PapagoService {
         this.validate(source, target, text);
 
         const url = 'https://openapi.naver.com/v1/papago/n2mt';
-        const result = await this.axiosService.post(url, {
-            source: source,
-            target: target,
-            text: text,
-        },
-        { headers:
-            {
-                'X-Naver-Client-Id': this.configService.get<string>('NAVER_CLIENT_ID'),
-                'X-Naver-Client-Secret': this.configService.get<string>('NAVER_CLIENT_SECRET'),
-            }
-        });
-        return result.data.message.result.translatedText;
+        try {
+            const result = await this.axiosService.post(url, {
+                source: source,
+                target: target,
+                text: text,
+            },
+            { headers:
+                {
+                    'X-Naver-Client-Id': this.configService.get<string>('NAVER_CLIENT_ID'),
+                    'X-Naver-Client-Secret': this.configService.get<string>('NAVER_CLIENT_SECRET'),
+                }
+            });
+            return result.data.message.result.translatedText;
+        } catch (err) {
+            throw new Error('번역 요청 중 오류가 발생했습니다.');
+        }
     }
 
     validate(source: string, target: string, text: string) {
-        const languageCode = ['ko', 'en', 'ja', 'zh-CN', 'zh-TW'];
         if (!languageCode.includes(source))
-            throw 'source 속성의 언어 코드가 유효하지 않습니다.';
+            throw new Error('source 속성의 언어 코드가 유효하지 않습니다.');
         if(!languageCode.includes(target))
-            throw 'target 속성의 언어 코드가 유효하지 않습니다.';
+            throw new Error('target 속성의 언어 코드가 유효하지 않습니다.');
         if(text.trim() === "")
-            throw 'text 속성에 유효한 문자열이 입력되어야 합니다.';
+            throw new Error('text 속성에 유효한 문자열이 입력되어야 합니다.');
     }
 }
