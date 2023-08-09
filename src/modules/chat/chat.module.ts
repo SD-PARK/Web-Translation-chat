27c4f62afa87e6f141a26eb733484862be80cbdf
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-08-09 12:52:09 +0900

    remove: gateway 임시 제거

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
-import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { TypeOrmExModule } from 'src/config/typeorm_ex/typeorm_ex.module';
 import { ChatMessageRepository } from './chat_messages/chat_messages.repository';
@@ -11,6 +10,6 @@ import { ChatRoomRepository } from './chat_rooms/chat_rooms.repository';
     TypeOrmExModule.forCustomRepository([ChatMessageRepository, ChatRoomRepository]),
   ],
   controllers: [ChatController],
-  providers: [ChatGateway, ChatService]
+  providers: [ChatService]
 })
 export class ChatModule {}
