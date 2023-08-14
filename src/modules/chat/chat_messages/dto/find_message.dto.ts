48275dd991cdccb81cfa1a4c8facb8bcdd05ed2f
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-08-14 15:22:27 +0900

    refactor: Query용 Validate 추가

--- src/modules/chat/chat_messages/dto/find_message.dto.ts
+++ src/modules/chat/chat_messages/dto/find_message.dto.ts
@@ -1,14 +1,18 @@
-import { IsDate, IsNumber, Min } from "class-validator";
+import { Transform } from "class-transformer";
+import { IsDate, IsNumber, IsOptional } from "class-validator";
+import { toDate, toNumber } from 'src/config/helper/cast.helper'
 
 export class FindMessageDto {
+    @Transform(({ value }) => toNumber(value, { min: 0 }))
     @IsNumber()
-    @Min(0)
     room_id: number;
 
+    @Transform(({ value }) => toDate(value))
     @IsDate()
     send_at: Date;
 
+    @Transform(({ value }) => toNumber(value, { default: 20, min: 0}))
+    @IsOptional()
     @IsNumber()
-    @Min(0)
     take: number;
 }
