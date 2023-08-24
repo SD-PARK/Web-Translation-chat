fd8f19dbefe4accf14efd81d820453b702861c29
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-08-25 02:24:27 +0900

    feat: 정적 파일 및 뷰 엔진 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // 정적 파일 경로 설정
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  // 뷰 엔진 설정
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
