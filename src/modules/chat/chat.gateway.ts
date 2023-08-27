555056a36145ac3814f88d341916b019f18a6fde
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-08-27 15:15:39 +0900

    feat: 채팅 관련 소켓 추가

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -1,19 +1,84 @@
 import { Logger } from '@nestjs/common';
 import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Namespace, Socket } from 'socket.io';
+import { CreateMessageDto } from './chat_messages/dto/create_message.dto';
+import { ChatService } from './chat.service';
+import { FindMessageDto } from './chat_messages/dto/find_message.dto';
+import { ChatMessage } from './chat_messages/chat_messages.entity';
+import { PapagoService } from 'src/api/papago/papago.service';
 
 @WebSocketGateway({
   namespace: 'chat',
-  cors: {
-    origin: ['http://localhost:3000'],
-  },
+  cors: { origin: '*' },
 })
 export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly papagoService: PapagoService,
+  ) {}
+
   private readonly logger = new Logger(ChatGateway.name);
 
   // namespace를 설정하지 않으면 @WebSocketServer는 서버 인스턴스를 반환함; @WebSocketServer() server: Socket
   @WebSocketServer() nsp: Namespace;
+
+  // 접속 시 room에 join 시키고 해당 room의 메시지 불러와서 돌려줌.
+  @SubscribeMessage('join')
+  async handleJoin(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() joinData: FindMessageDto,
+  ) {
+    const roomIdString = joinData.room_id.toString();
+    socket.join(roomIdString);
+    this.nsp.to(roomIdString).emit('person-update', [ ...this.nsp.adapter.rooms.get(roomIdString) ]);
+    return {
+      room_data: await this.chatService.findOneRoom(joinData.room_id),
+      message_data: await this.chatService.findMessage(joinData),
+    };
+  }
+
+  // room 나가면 leave 처리
+  @SubscribeMessage('leave')
+  handleLeave(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() roomId: number,
+  ) {
+    const roomIdString = roomId.toString();
+    socket.leave(roomIdString);
+  }
+
+  // 메시지 송신 시 DB에 더하고 같은 room의 사람들에게 전송 (미번역 상태)
+  @SubscribeMessage('message')
+  async handleMessage(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() data: CreateMessageDto,
+  ) {
+    const roomIdString = data.room_id.toString();
+    const message: ChatMessage = await this.chatService.createMessage(data);
+    this.nsp.to(roomIdString).emit('message', message);
+  }
+
+  // 번역 요청 시 번역 상태 확인 및(이미 번역되어 있으면 그대로 반환) 번역 후 DB 저장, 반환
+  @SubscribeMessage('reqTranslate')
+  async handleReqTranslate(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() data: {
+      message_id: number,
+      language: string,
+    }
+  ) {
+    const message: ChatMessage = await this.chatService.findOneMessage(data.message_id);
+    if (message[`${data.language}_text`]) return message;
+
+    const tMessage: string = await this.papagoService.translate(message.language, data.language, message.message_text);
+    await this.chatService.updateMessage(data.message_id, {
+      [`${data.language}_text`]: tMessage,
+    });
+    message[`${data.language}_text`] = tMessage;
+
+    return message;
+  }
 
   // 초기화 이후
   afterInit() {
@@ -26,6 +91,7 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     });
 
     this.nsp.adapter.on('leave-room', (room, id) => {
+      this.nsp.to(room).emit('person-update', [ ...this.nsp.adapter.rooms.get(room) ]);
       this.logger.log(`[Socket: ${id}]가 [Room: ${room}]에서 나갔습니다.`);
     });
 
@@ -39,23 +105,10 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   // 소켓 연결 시
   handleConnection(@ConnectedSocket() socket: Socket) {
     this.logger.log(`${socket.id} 소켓 연결`);
-
-    // socket.broadcast.emit('message', {
-    //   message: `${socket.id}가 참여하였습니다.`,
-    // });
   }
 
   // 소켓 연결 종료 시
   handleDisconnect(@ConnectedSocket() socket: Socket) {
     this.logger.log(`${socket.id} 소켓 연결 ❌`);
-  }
-
-  @SubscribeMessage('message')
-  handleMessage(
-    @ConnectedSocket() socket: Socket,
-    @MessageBody() message: string,
-  ) {
-    socket.broadcast.emit('message', { username: socket.id, message });
-    return { username: socket.id, message };
   }
 }
--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -5,9 +5,11 @@ import { TypeOrmExModule } from 'src/config/typeorm_ex/typeorm_ex.module';
 import { ChatMessageRepository } from './chat_messages/chat_messages.repository';
 import { ChatRoomRepository } from './chat_rooms/chat_rooms.repository';
 import { ChatGateway } from './chat.gateway';
+import { PapagoModule } from 'src/api/papago/papago.module';
 
 @Module({
-  imports: [
+  imports: [ 
+    PapagoModule,
     TypeOrmExModule.forCustomRepository([ChatMessageRepository, ChatRoomRepository]),
   ],
   controllers: [ChatController],
