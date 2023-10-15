10d8396708aa4e016221a3fa37e5e8c09ce1802f
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-10-15 15:59:22 +0900

    test: cookie test

--- public/js/search-language.js
+++ public/js/search-language.js
@@ -39,12 +39,10 @@ setLanguage();
 function setCookie(name, value, exp) {
     var date = new Date();
     date.setTime(date.getTime() + exp*24*60*60*1000);
-    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/' + ';domain=papago-chat.site';
-    console.log(`setCookie: ${name}, ${value}`);
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
 };
 
 function getCookie(name) {
     var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    console.log(document.cookie);
     return value? value[2] : null;
 }
