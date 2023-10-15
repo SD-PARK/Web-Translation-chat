cbdd848c55df8dc5f2c785f07926620cb566f780
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-10-15 15:53:48 +0900

    test: cookie test

--- public/js/search-language.js
+++ public/js/search-language.js
@@ -39,11 +39,12 @@ setLanguage();
 function setCookie(name, value, exp) {
     var date = new Date();
     date.setTime(date.getTime() + exp*24*60*60*1000);
-    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/' + ';domain=papago-chat.site';
     console.log(`setCookie: ${name}, ${value}`);
 };
 
 function getCookie(name) {
     var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    console.log(document.cookie);
     return value? value[2] : null;
 }
