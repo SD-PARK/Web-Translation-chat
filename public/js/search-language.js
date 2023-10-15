42d80cea39e06ee6eac9ea62d5ee7ffe16df2fed
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-10-15 16:37:13 +0900

    test: cookie test

--- public/js/search-language.js
+++ public/js/search-language.js
@@ -39,7 +39,8 @@ setLanguage();
 function setCookie(name, value, exp) {
     var date = new Date();
     date.setTime(date.getTime() + exp*24*60*60*1000);
-    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/' + ';SameSite=None;';
+    console.log(document.cookie);
     console.log(getCookie(name));
 };
 
