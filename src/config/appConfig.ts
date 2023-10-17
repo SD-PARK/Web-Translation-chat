78eba8f7fdb39624b37f225628cba647f20ef9e3
SD-PARK <97375357+SD-PARK@users.noreply.github.com>
2023-10-17 14:05:38 +0900

    fix: 현재 사용가능한 언어만 배열에 입력

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,3 +1,4 @@
 export default {
-    supportedLanguage: ['ko', 'en', 'ja', 'zh-CN', 'zh-TW', 'vi', 'id', 'th', 'th', 'de', 'ru', 'es', 'it', 'fr'],
+    supportedLanguage: ['ko', 'en', 'ja'],
+    // supportedLanguage: ['ko', 'en', 'ja', 'zh-CN', 'zh-TW', 'vi', 'id', 'th', 'th', 'de', 'ru', 'es', 'it', 'fr'],
 }
